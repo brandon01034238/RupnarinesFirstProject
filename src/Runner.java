1a113d13889fadea91da3076cf28d3a4e089b9be
BT_1N3_01 <sample@bths.edu>
2024-10-25 10:06:57 -0400

    changed x to cats and added an if else statement

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,13 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
-        int x = 5;
-        System.out.println("x is: " + x);
+        int cats = 5;
+        System.out.println("Jack has " + cats + "cats" );
+        cats ++;
+        if (cats == 6) {
+            System.out.println("Jack has a new cat");
+        } else {
+            System.out.println("Cats are Cool");
+        }
     }
 }
